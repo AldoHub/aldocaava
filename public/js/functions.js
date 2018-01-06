040b2e99c694ba3558d2f2c4870ab0270600f1c0
AldoHub <aldocaava@hotmail.com>
2018-01-05 20:01:08 -0500

    style changes made

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,23 +1,14 @@
 $(function(){
-    $("#menu-bars").on("click", ()=>{
-        $("#menu").next("UL").toggle();
-        $("nav").css({"box-shadow": "0px 2px 2px #555"}); 
-        $("#menu").next("UL").css({"display": "inline-table"});
-       
-        if($("#menu-bars").hasClass("fa fa-bars")){
-            $("#menu-bars").removeClass("fa fa-bars");
-            $("#menu-bars").addClass("fa fa-times");
-        }else {
-            $("#menu").next("UL").css({"display": "none"})
-            $("nav").css({"box-shadow": ""});
-            $("#menu-bars").removeClass("fa fa-times");
-            $("#menu-bars").addClass("fa fa-bars");
-         
-        }
         
-        
-    })
-        
+   
+        $('#nav-icon').click(function(){
+          $(this).toggleClass('open');
+          $("#menu").next("UL").toggle();  
+   
+        });
+      
+    
+
     $(window).on("scroll", ()=>{
        if($(window).scrollTop() > 5){
         $("nav").css({"box-shadow": "0px 2px 2px #555"}); 
@@ -28,10 +19,7 @@ $(function(){
 
     if(window.innerWidth <= 849){
        $("#menu").next("UL").hide(); 
-       if($("#menu-bars").hasClass("fa fa-times")){
-            $("#menu-bars").removeClass("fa fa-times");
-            $("#menu-bars").addClass("fa fa-bars");
-        }
+       
     }
     if(window.innerWidth > 849){
         $("#menu").next("UL").show(); 
@@ -42,12 +30,8 @@ $(function(){
     $(window).on("resize", ()=>{
         if(window.innerWidth <= 849){
             $("#menu").next("UL").hide(); 
-          
-            if($("#menu-bars").hasClass("fa fa-times")){
-                $("#menu-bars").removeClass("fa fa-times");
-                $("#menu-bars").addClass("fa fa-bars");
-            }
-         }
+            $("#nav-icon").removeClass("open");
+        }
          if(window.innerWidth > 849){
              $("#menu").next("UL").show(); 
               
