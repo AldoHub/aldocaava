09c497ff7eae59553a426423e9446e84d502d724
AldoHub <aldocaava@hotmail.com>
2018-01-15 16:27:37 -0500

    changed style

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,41 +1,19 @@
 $(function(){
-        
-   
-        $('#nav-icon').click(function(){
-          $(this).toggleClass('open');
-          $("#menu").next("UL").toggle();  
-   
-        });
-      
-    
 
     $(window).on("scroll", ()=>{
-       if($(window).scrollTop() > 5){
-        $("nav").css({"box-shadow": "0px 2px 2px #555"}); 
+       if($(window).scrollTop() > 1){
+        $("nav").css({"box-shadow": "0px 2px 2px #000"}); 
        }else{
         $("nav").css({"box-shadow": ""});
        }
     }); 
 
-    if(window.innerWidth <= 849){
-       $("#menu").next("UL").hide(); 
-       
-    }
-    if(window.innerWidth > 849){
-        $("#menu").next("UL").show(); 
-                 
-     }
 
 
-    $(window).on("resize", ()=>{
-        if(window.innerWidth <= 849){
-            $("#menu").next("UL").hide(); 
-            $("#nav-icon").removeClass("open");
-        }
-         if(window.innerWidth > 849){
-             $("#menu").next("UL").show(); 
-              
-          }
-             
-    }); 
+
+    $("body").on("click", "#nav-icon", ()=>{
+        $("#nav-icon").toggleClass("open");
+        $("nav ul:last-of-type").toggleClass("hide", 50, "linear");     
+    });
+
 });
